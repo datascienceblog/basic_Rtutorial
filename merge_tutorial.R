54086fef541ab102a08e0393134b931cd50a7802
Dusan Ku <d.ku@auckland.ac.nz>
2020-04-10 22:11:44 +0900

    Revision

--- merge_tutorial.R
+++ merge_tutorial.R
@@ -1,3 +1,5 @@
+# Example 1
+
 data1 <- data.frame(id = 1:6,                                  # Create first example data frame
                     x1 = c(5, 1, 4, 9, 1, 2),
                     x2 = c("A", "Y", "G", "F", "G", "Y"))
@@ -6,7 +8,23 @@ data2 <- data.frame(id = 4:9,                                  # Create second e
                     y1 = c(3, 3, 4, 1, 2, 9),
                     y2 = c("a", "x", "a", "x", "a", "x"))
 
+# Example 2
+
 merge(data1, data2, by = "id")       # Merge data frames by columns names
 
 merge(data1, data2, by = "id", all.x = TRUE)  # Keep all rows of x-data
 
+merge(data1, data2, by = "id", all.y = TRUE)                   # Keep all rows of y-data
+
+merge(data1, data2, by = "id", all.x = TRUE, all.y = TRUE)     # Keep all rows of both data frames
+
+
+# Example 3
+
+data3 <- data.frame(id = 5:6,                                  # Create third example data frame
+                    z1 = c(3, 2),
+                    z2 = c("K", "b"))
+
+data12 <- merge(data1, data2, by = "id")                       # Merge data 1 & 2 and store
+
+merge(data12, data3, by = "id")                                # Merge multiple data frames
